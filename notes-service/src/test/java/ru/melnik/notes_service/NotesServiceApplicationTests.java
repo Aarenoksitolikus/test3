18c738c8296f89f7697ad28979788f5f08c901be
Oleg Melnik <melnikoleg@MacBook-Pro.local>
2026-03-28 16:16:11 +0300

    init spring boot project

---
+++ notes-service/src/test/java/ru/melnik/notes_service/NotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.melnik.notes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
