2c4a53bd4585dd8473f488b322904e97ba5f98e8
PalmaDae <arturd277@mail.ru>
2026-03-28 16:40:30 +0300

    Работает?

--- src/main/java/ru/itis/fisd_cw/controller/NoteController.java
+++ src/main/java/ru/itis/fisd_cw/controller/NoteController.java
@@ -3,12 +3,15 @@ package ru.itis.fisd_cw.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.itis.fisd_cw.data.dto.NoteDto;
 import ru.itis.fisd_cw.data.entity.NoteEntity;
 import ru.itis.fisd_cw.service.NoteService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/notes")
@@ -19,6 +22,27 @@ public class NoteController {
     @GetMapping("/{id}")
     public ResponseEntity<NoteEntity> getById(@PathVariable Long id) {
         return ResponseEntity.ok(noteService.getById(id));
+    }
+
+    @GetMapping("/title")
+    public ResponseEntity<List<NoteEntity>> getByTitle(@RequestParam String title) {
+        return ResponseEntity.ok(noteService.getByTitle(title));
+    }
+
+    @GetMapping("/content")
+    public ResponseEntity<List<NoteEntity>> getByContent(@RequestParam String content) {
+        return ResponseEntity.ok(noteService.getByContent(content));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<NoteEntity> update(@PathVariable Long id, @RequestBody NoteDto dto) {
+        return ResponseEntity.ok(noteService.updateNote(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        noteService.deleteNote(id);
+        return ResponseEntity.noContent().build();
     }
 
 
